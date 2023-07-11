76cc4f8d935921530afdbcfe08adfcb661cebc88
Sadeka <sadeka114922@gmail.com>
2023-07-11 10:58:39 +0600

    commit

--- database/migrations/2023_07_10_183754_create_generals_table.php
+++ database/migrations/2023_07_10_183754_create_generals_table.php
@@ -15,27 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('home_tittle');
             $table->string('home_des');
-            $table->string('msg_title');
-            $table->string('msg_author');
-            $table->text('msg_desc');
             $table->string('gallery_des');
             $table->string('news_des');
-            $table->string('client_des');
             $table->string('member_des');
-            $table->string('about_des');
             $table->string('service_des');
-
             $table->string('logo')->nullable();
-            $table->string('mobile');
-            $table->string('email');
-            $table->string('address');
-            $table->text('map');
-            $table->string('facebook');
-            $table->string('twitter');
-            $table->string('linkedin');
-            $table->string('instragam');
             $table->boolean('status')->default(false);
-            $table->timestamp('created_at')->useCurrent();
+            $table->timestamps();
         });
     }
 
